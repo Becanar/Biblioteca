fe26093314611a6ad1508999c13edf389c1249b7
Becanar <116558378+Becanar@users.noreply.github.com>
2025-01-29 17:46:29 +0100

    alumnos fxml añadido

---
+++ src/main/java/com/benat/cano/biblioteca/controller/AlumnosController.java
@@ -0,0 +1,44 @@
+package com.benat.cano.biblioteca.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AlumnosController {
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Label lblDelete;
+
+    @FXML
+    private TextField txtAnio;
+
+    @FXML
+    private TextField txtCiudad;
+
+    @FXML
+    private TextField txtCiudad1;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
+
+}
