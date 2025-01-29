271f787f41bc7070e66a104a9c4cefc5aab27f6a
Becanar <116558378+Becanar@users.noreply.github.com>
2025-01-29 21:02:49 +0100

    añado prestamos.fxml y controlle

---
+++ src/main/java/com/benat/cano/biblioteca/controller/PrestamosController.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ListView<?> lstDeportista;
+
+    @FXML
+    private ListView<?> lstEquipo;
+
+    @FXML
+    private TextField txtEdad;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
+
+}
